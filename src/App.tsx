cbb73dffd68becec30531e25e00ebdfd89cb86ee
SocioHubv01 <deepakdevi800@gmail.com>
2024-04-18 18:31:29 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,43 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { Authenticated, GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
-import { useNotificationProvider } from "@refinedev/antd";
+import { Edit, useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
-import dataProvider, {
-  GraphQLClient,
-  liveProvider,
-} from "@refinedev/nestjs-query";
+import { authProvider, dataProvider,liveProvider } from "./providers";
+import { Home , ForgotPassword , Login , Register, CompanyList   } from "./pages"
 import routerBindings, {
+  CatchAllNavigate,
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import { App as AntdApp } from "antd";
-import { createClient } from "graphql-ws";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { authProvider } from "./authProvider";
-import { ColorModeContextProvider } from "./contexts/color-mode";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout";
+import { resources } from "./config/resources";
+import Create from "./pages/company/create";
+import EditPage from "./pages/company/edit";
+import List from "./pages/tasks/list";
+import CreateTask from "./pages/tasks/create";
+import EditTask from "./pages/tasks/edit";
 
-const API_URL = "https://api.nestjs-query.refine.dev/graphql";
-const WS_URL = "wss://api.nestjs-query.refine.dev/graphql";
 
-const gqlClient = new GraphQLClient(API_URL);
-const wsClient = createClient({ url: WS_URL });
 
 function App() {
   return (
     <BrowserRouter>
       <GitHubBanner />
       <RefineKbarProvider>
-        <ColorModeContextProvider>
           <AntdApp>
             <DevtoolsProvider>
               <Refine
-                dataProvider={dataProvider(gqlClient)}
-                liveProvider={liveProvider(wsClient)}
+                dataProvider={dataProvider}
+                liveProvider={liveProvider}
                 notificationProvider={useNotificationProvider}
                 routerProvider={routerBindings}
                 authProvider={authProvider}
+                resources={resources}
                 options={{
                   syncWithLocation: true,
                   warnWhenUnsavedChanges: true,
@@ -48,7 +47,35 @@ function App() {
                 }}
               >
                 <Routes>
-                  <Route index element={<WelcomePage />} />
+                  <Route index path="/register" element={<Register />}/>
+                  <Route index path="/login" element={<Login />}/>
+                  <Route index path="/forgot-password" element={<ForgotPassword />}/>
+                  <Route element={
+                  <Authenticated
+                  key="authenticated-layout"
+                  fallback={<CatchAllNavigate to="/login"/>}
+                  >
+                    <Layout>
+                      <Outlet/>
+                    </Layout>
+
+
+                  </Authenticated>
+                  }>
+                    <Route index element={<Home />}/>
+                    <Route path="/companies">
+                      <Route index element={<CompanyList/>}/>
+                      <Route path="new" element={<Create/>}/>
+                      <Route path="edit/:id" element={<EditPage/>}/>
+                    </Route>
+                    <Route path="/tasks" element={
+                    <List>
+                      <Outlet/>
+                    </List>}>
+                      <Route path="new" element={<CreateTask/>}/>
+                      <Route path="edit/:id" element={<EditTask/>}/>
+                    </Route>
+                </Route>
                 </Routes>
                 <RefineKbar />
                 <UnsavedChangesNotifier />
@@ -57,7 +84,6 @@ function App() {
               <DevtoolsPanel />
             </DevtoolsProvider>
           </AntdApp>
-        </ColorModeContextProvider>
       </RefineKbarProvider>
     </BrowserRouter>
   );
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,18 @@
-export { Header } from "./header";
+import UpcomingEvents from "./home/upcoming-events";
+import DealsChart from "./home/deals-chart";
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+import AccordionHeaderSkeleton from "./skeleton/accordion-header";
+import KanbanColumnSkeleton from "./skeleton/kanban";
+import ProjectCardSkeleton from "./skeleton/project-card";
+import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+import DashboardTotalCountCard from "./home/total-count-card";
+import LatestActivities from "./home/latest-activities";
+export {UpcomingEvents,DealsChart,UpcomingEventsSkeleton,
+AccordionHeaderSkeleton,KanbanColumnSkeleton,ProjectCardSkeleton,LatestActivitiesSkeleton,DashboardTotalCountCard,LatestActivities};
+export * from "./tasks/kanban/form/description";
+export * from "./tasks/kanban/form/due-date";
+export * from "./tasks/kanban/form/stage";
+export * from "./tasks/kanban/form/title";
+export * from "./tasks/kanban/form/users";
+export * from "./tasks/kanban/form/header";
+export * from "./accordion";
